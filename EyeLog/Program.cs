159b985578b3945be649b69a562fb91eca9770fc
Ivan Bakaidov <ibakaidov@ya.ru>
2023-03-23 17:21:53 +0300

    add files

---
+++ EyeLog/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tobii.Interaction;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeLog
+{
+    internal class Program
+    {
+        private static Host host;
+
+        static void Main(string[] args)
+        {
+            host = new Host();
+            host.Streams.CreateGazePointDataStream()
+                .Next += OnGazePoint;
+            Console.ReadKey();
+        }
+
+        private static void OnGazePoint(object sender, StreamData<GazePointData> e)
+        {
+            Console.WriteLine(Math.Round( e.Data.X) + "," + Math.Round(e.Data.Y) + ","+ Math.Round (e.Data.Timestamp));
+
+        }
+    }
+}
