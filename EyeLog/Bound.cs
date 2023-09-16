564abc1521a2e46951dc0f56d27aac603c2949df
Ivan Bakaidov <ibakaidov@ya.ru>
2023-09-16 08:41:11 +0300

    change logic of eyelog

---
+++ EyeLog/Bound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeLog
+{
+    internal class Bound
+    {
+        int x;
+        int y;
+        int width;
+        int height;
+        public Bound(int x, int y, int width, int height)
+        {
+            this.x = x;
+            this.y = y;
+            this.width = width;
+            this.height = height;
+        }
+
+        public bool isInside(int x, int y)
+        {
+            return (this.x<x && this.x+width>x)&&(this.y<y && this.y+height>y);
+        }
+    }
+}
--- EyeLog/Program.cs
+++ EyeLog/Program.cs
@@ -1,29 +1,156 @@
 ﻿using Tobii.Interaction;
 
-using System;
+using System ;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
+using Tobii.Interaction.Framework;
+using System.Runtime.CompilerServices;
 
 namespace EyeLog
 {
     internal class Program
     {
         private static Host host;
+        private static Bound[] bounds = new Bound[0];
+        private static StreamData<GazePointData> CurrentEyeValue;
+        private static long lastProcessedTS;
+        private static long ts;
+        private static long lastExitStartTS=-1;
+        private static long lastEnterTS;
+        private static int lastSelected;
+        private static long CLICK_TIMEOUT = 1000;
+        private static long lastClicksCount;
 
         static void Main(string[] args)
         {
             host = new Host();
-            host.Streams.CreateGazePointDataStream()
-                .Next += OnGazePoint;
-            Console.ReadKey();
+            host.EnableConnection();
+            host.Streams.CreateGazePointDataStream().Next += OnGaze;  
+            new Thread(new ThreadStart(InputCycle)).Start();
+            new Thread(new ThreadStart(EyeCycle)).Start();
         }
 
-        private static void OnGazePoint(object sender, StreamData<GazePointData> e)
+        private static void EyeCycle()
         {
-            Console.WriteLine(Math.Round( e.Data.X) + "," + Math.Round(e.Data.Y) + ","+ Math.Round (e.Data.Timestamp));
+            
+            while (true)
+            {
+                if(CurrentEyeValue == null) { continue; }
 
+                var now = DateTime.Now.ToFileTime();
+
+                if(lastProcessedTS == ts) {
+                    continue;
+                }
+
+                int nowInside = -1;
+                for (int i = 0; i < bounds.Length; i++)
+                {
+                    bool isInside = bounds[i].isInside(((int) CurrentEyeValue.Data.X), ((int)CurrentEyeValue.Data.Y));
+                    if(isInside)
+                    {
+                        nowInside = i;
+                    }
+                }
+
+
+                var isEnter = lastSelected == -1 && nowInside!=-1 && lastSelected!=nowInside; 
+                var isExit = lastSelected != nowInside;
+                var isStay =  lastSelected != -1 && lastSelected==nowInside;
+
+                if (isEnter)
+                {
+                    OnEnter(nowInside, ts);
+                }
+                if (isExit)
+                {
+                    OnExit(now);   
+                }
+                if (isStay)
+                {
+                    lastExitStartTS = -1;
+                    OnStay(ts);
+                }
+                lastProcessedTS = ts;
+
+            }
         }
+
+        private static void OnStay(long ts)
+        {
+            long insideTime = (ts - lastEnterTS) / 10000;
+            long clicks = insideTime / CLICK_TIMEOUT;
+            if (lastClicksCount != clicks)
+            {
+                lastClicksCount = clicks;
+                if (clicks > 0) Console.WriteLine("click:" + lastSelected + "," + clicks);
+            }
+        }
+
+        private static void OnExit(long ts)
+        {
+            if (lastExitStartTS == -1)
+            {
+                lastExitStartTS = ts;
+            }
+            else if(ts - lastExitStartTS > 50)  
+            {
+                lastSelected = -1;
+                lastExitStartTS = -1;
+                Console.WriteLine("exit");
+            }
+        }
+
+        private static void OnEnter(int nowInside, long ts)
+        {
+            lastEnterTS = ts;
+            lastSelected = nowInside;
+            Console.WriteLine("enter:" + lastSelected);
+        }
+
+        private static void InputCycle()
+        {
+
+            while (true)
+            {
+                try
+                {
+                    var line = Console.ReadLine();
+                    if (line.StartsWith("timeout:"))
+                    {
+                        CLICK_TIMEOUT = int.Parse(line.Substring("timeout:".Length));
+
+                        continue;
+                    }
+                    var robjs = line.Split(';');
+                    bounds = new Bound[robjs.Length];
+                    lastSelected = -1;
+                    for (int i = 0; i < robjs.Length; i++)
+                    {
+                        string robj = robjs[i];
+                        var rcoords = robj.Split(',');
+                        var x = int.Parse(rcoords[0]);
+                        var y = int.Parse(rcoords[1]);
+                        var width = int.Parse(rcoords[2]);
+                        var height = int.Parse(rcoords[3]);
+                        bounds[i] = new Bound(x, y, width, height);
+                    }
+                }
+                catch
+                {
+                    bounds = new Bound[0];
+                }
+            }
+        }
+
+        private static void OnGaze(object sender, StreamData<GazePointData> e)
+        {
+
+            CurrentEyeValue = e;
+            ts = DateTime.Now.ToFileTime();
+        }
+        
     }
+
 }
